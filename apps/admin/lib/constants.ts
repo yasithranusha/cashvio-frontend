d0129edebbbbe92de2cccead391325b1fe2a13b3
Yasith Ranusha Silva <yasithranusha24@gmail.com>
2025-03-30 23:49:40 +0530

    fix: build errors

--- apps/admin/lib/constants.ts
+++ apps/admin/lib/constants.ts
@@ -1,13 +1,2 @@
-export const BACKEND_URL =
-  process.env.API_BASE_URL ??
-  (() => {
-    throw new Error("API_BASE_URL environment variable is not defined");
-  })();
-
-export const SESSION_SECRET =
-  process.env.SESSION_SECRET_KEY_ADMIN ??
-  (() => {
-    throw new Error(
-      "SESSION_SECRET_KEY_ADMIN environment variable is not defined"
-    );
-  })();
+export const BACKEND_URL = process.env.API_BASE_URL!;
+export const SESSION_SECRET = process.env.SESSION_SECRET_KEY_ADMIN!;
